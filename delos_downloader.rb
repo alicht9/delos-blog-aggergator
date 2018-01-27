b26d863f89c03613d19a414a453b68097d1d8912
alicht9 <alicht9@gmail.com>
2018-01-27 01:24:24 +0000

    Hack around a bug on the delos site
    I am probably missing out on page 10 somewhere here
    I don't think the delos site will load it anyways
    This is annoying.

--- delos_downloader.rb
+++ delos_downloader.rb
@@ -4,8 +4,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'prawn'
 
-LAST_PAGE=18
-BASE_URL = 'https://svdelos.com/travel-blogs/page/PAGE/?order=ASC'
+BASE_URL = 'https://svdelos.com/travel-blogs/page/PAGE/?order=SORT'
 PAGE_URL= 'https://svdelos.com/travel-blogs/fixing-stuff-remote-places-brian/'
 FILE = 'delos-blogs.pdf'
 IMAGE_SCALE= 0.5
@@ -36,20 +35,47 @@ def create_pdf data
   end
 end
 
-
-def get_blog_links base_url
+def crawl_blog_links base_url, direction
   blog_urls = Array.new
-  (1...LAST_PAGE).each do |page|
+  
+  range = nil
+  if direction == "ASC"
+    range = 1.upto(9)
+  else
+    range = 8.downto(1)
+  end
+  range.each do |page|
+
     this_page = base_url.gsub("PAGE", page.to_s)
+    this_page.gsub!("SORT", direction)
     index_page = Nokogiri::HTML((open(this_page)))
     page_urls = index_page.css(".entry-title").collect do |blog_p_tag|
       blog_p_tag.children.first.attr("href")
     end
-    #first one is latest
+    #first one is always the latest
     page_urls.shift
     blog_urls << page_urls
   end
   return blog_urls.flatten
+end
+
+def get_first_10_blog_links url
+  crawl_blog_links(url, "ASC")
+end
+
+def get_last_10_blog_links url
+  crawl_blog_links(url, "DESC")
+end
+
+def get_blog_links url
+  #ok... this is really dumb
+  #The delos site has a bug where it only pays attention
+  #to the last digit of the page number in the URL
+  #ex: 6 = 6  16 = 6   1113426 = 6
+  #Thankfully there are less than 20 pages, so we can just
+  #flip the sort for now... this won't work forever
+  #Gunna email Capt' Breeeyawn bout that one
+  return [get_first_10_blog_links(url), get_last_10_blog_links(url)].flatten
 end
 
 if __FILE__ == $0 
